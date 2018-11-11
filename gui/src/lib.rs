18a2e9f41b0905955963167f5fb47beee245d954
Jon Lamb <lamb.jon.io@gmail.com>
2018-11-11 09:35:09 -0800

    Stub out gui lib

---
+++ gui/src/lib.rs
@@ -0,0 +1,11 @@
+#![no_std]
+
+extern crate embedded_graphics;
+extern crate rgb;
+
+use embedded_graphics::drawable::Pixel;
+use embedded_graphics::pixelcolor::PixelColor;
+use embedded_graphics::Drawing;
+use rgb::RGB8;
+
+// TODO
